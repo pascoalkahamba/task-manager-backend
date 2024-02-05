f4ecae5751dfa0bf854c65db57845f5de9f08528
pascoalkahamba <pascoalkahamba25@gmail.com>
2024-02-05 13:59:43 +0100

    I've fixed some mistake about create new note

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import "./config/dbConfig";
-import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const routes = require("./routes");
 const app: Express = express();
-
+app.use(bodyParser.json());
+app.use(cors());
 app.use(routes);
 app.listen(3000);
